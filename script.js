6429d424075d273e62aad4f4ae88c6c17a974b9c
Tyler Shanks <86684183+tylershanks@users.noreply.github.com>
2021-09-15 14:35:34 -0400

    Add files via upload
    It is all done. Added the color changing of the grid with black, rainbow, erase, and reset. It doesn't look perfect but I am looking to move on and learn more.

--- script.js
+++ script.js
@@ -2,13 +2,69 @@ console.log("this is gonna be a grid");
 
 
 const columnContainer = document.querySelector('.gridColumn');
+const square = document.querySelector("div");
 
 let slider = document.getElementById('myRange');
 let cellSqrt = 16;
 let isDrawing = false;
+let color = "black";
+let rainbowColor = false;
 
+document.getElementById("reset").addEventListener("click", reset);
+document.getElementById("black").addEventListener("click", black);
+document.getElementById("rainbow").addEventListener("click", rainbow);
+document.getElementById("erase").addEventListener("click", erase);
+
+//calls to redraw the whole grid which also resets the colors
+function reset() { 
+    createCells(cellSqrt);
+}
+
+function black() {
+    rainbowColor = false;
+    color = "black";
+}
+
+//sets rainbow to true, easier to have desired effect
+function rainbow() { 
+    rainbowColor = true;
+}
+
+function erase() {
+    rainbowColor = false;
+    color = "gray";
+}
+
+//adds the function of only drawing on mouse down, definitely not perfect but I think it's better than 
+//drawing on hover only
+document.body.onmousedown = function() { 
+    isDrawing = true;
+    console.log(isDrawing);
+}
+
+document.body.onmouseup = function() {
+    isDrawing = false;
+    console.log(isDrawing);
+}
+
+//changes color of individual cells. Also checks if rainbowColor is true or false
+square.addEventListener("mouseover", function(e) {
+    if (e.target.matches('.gridCell') && isDrawing == true && rainbowColor == false) {
+    console.log("pls");
+    e.target.style.backgroundColor=color;
+    }
+
+    else if (e.target.matches('.gridCell') && isDrawing == true && rainbowColor == true) {
+        console.log("pls");
+        color = "rgb("+Math.floor(Math.random()*256)+", "+Math.floor(Math.random()*256)+", "+Math.floor(Math.random()*256)+")";
+        e.target.style.backgroundColor=color;
+    }
+})
+
+//runs on initial startup so grid is populated by default
 createCells(cellSqrt);
 
+//changes grid every time the slider is changed
 slider.addEventListener("change", function () {
     cellSqrt = slider.value;
     console.log(cellSqrt + " cellSqrt");
@@ -17,18 +73,7 @@ slider.addEventListener("change", function () {
     document.getElementById('cellSliderNumber').innerHTML = cellSqrt + "x" + cellSqrt;
 })
 
-/*const gridCell = document.querySelector('.gridCell');
-gridCell.addEventListener("mousedown", function() {
-    gridCell.style.backgroundColor = 'blue';
-    console.log('color change');
-})*/
-
-function changeColor(divObj) {
-    divObj.style.backgroundColor = blue;
-}
-
-
-
+//creates a grid of divs inside the grid container. adjustable by the slider
 function createCells(cellsToCreate) { //loops through cellSqrt number of times and creates one row, populates it with however many cells, then does another row and populates etc.
 
     columnContainer.textContent = ""; //clears old cells
@@ -41,11 +86,9 @@ function createCells(cellsToCreate) { //loops through cellSqrt number of times a
             for (let j = 1; j <= cellsToCreate; j++) {
                 const gridCell = document.createElement("div")
                     gridCell.classList.add('gridCell');
-                    //gridCell.addEventListener("mousedown", console.log("cliiick"));
                     gridRow.appendChild(gridCell);
+                    gridCell.id = "gridCell";
+                    
             }
     }
 }
-
-
-
